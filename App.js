9f0e2cc87b19af4d9c548b95d540c8317119738b
musho777 <129147695+musho777@users.noreply.github.com>
2023-09-14 10:31:30 +0400

    level 1 game 5

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Level1_2 } from "./src/pages/Level1/level1_2"
 import { Level1_3 } from "./src/pages/Level1/level1_3"
 import { Level1_4 } from "./src/pages/Level1/level1_4"
 import { Level1_5 } from "./src/pages/Level1/level1_5"
-import { Level1_6 } from "./src/pages/Level1/level1_6"
+import { Level1_6 } from "./src/pages/Level1/level1_5"
 import { Level1_7 } from "./src/pages/Level1/level1_7"
 
 export default App = () => {
@@ -16,9 +16,9 @@ export default App = () => {
     {/* <Level1_1 /> */}
     {/* <Level1_2 /> */}
     {/* <Level1_3 /> */}
-    <Level1_4 />
+    {/* <Level1_4 /> */}
+    <Level1_5 />
     {/* <Level1_5 /> */}
-    {/* <Level1_6 /> */}
     {/* <Level1_7 /> */}
 
   </View>
